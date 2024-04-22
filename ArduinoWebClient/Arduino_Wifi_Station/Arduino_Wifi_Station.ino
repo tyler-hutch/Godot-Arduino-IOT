285548285bfc9bb85fe8a349962a428b42e46220
Tyler Hutchinson <hutchgamert@gmail.com>
2024-04-21 23:40:26 -0600

    ESP POST and Serial
    *ESP code to print information from a POST web request to the serial monitor
    *Folder cleanup

--- ArduinoWebClient/Arduino_Wifi_Station/Arduino_Wifi_Station.ino
+++ ArduinoWebClient/Arduino_Wifi_Station/Arduino_Wifi_Station.ino
@@ -8,6 +8,7 @@
    
 */
 #include <WiFi.h>
+#include <string.h>
 #include <ESPAsyncWebServer.h>
 #include <ArduinoJson.h> 
 
@@ -15,21 +16,33 @@ const char* wifi_network_ssid = "GoogleFiber";
 const char* wifi_network_password =  "goCougars";
  
 AsyncWebServer server(80);
- 
 
-// void handlePost() {
-//   StaticJsonDocument<200> doc;
-//   DeserializationError error = deserializeJson(doc, server.arg("plain"));
+String globalAction;
 
-//   if (error) {
-//     server.send(400, "text/plain", "Failed to parse JSON");
-//     return;
-//   }
+const char index_html[] PROGMEM = "<form action='action' method='post' id='myForm'>\
+<input type='submit' name='B1' value='B1' onclick='updateTextBox(this)'>\
+<input type='submit' name='B2' value='B2' onclick='updateTextBox(this)'>\
+<input type='text' id='textBox' readonly>\
+</form>\
+<script>\
+function updateTextBox(btn) {\
+  document.getElementById('textBox').value = btn.name;\
+}\
+</script>";
 
-//   String response;
-//   serializeJson(doc, response);
-//   server.send(200, "application/json", response);
-// }
+void action(AsyncWebServerRequest *request) {
+  Serial.println("ACTION!");
+
+  int params = request->params();
+  for (int i = 0; i < params; i++) {
+    AsyncWebParameter* p = request->getParam(i);
+    Serial.printf("POST[%s]: %s\n", p->name().c_str(), p->value().c_str());
+    if (p->name() == "action") {
+      globalAction = p->value(); // Store the post form action
+    }
+  }
+  request->send_P(200, "text/html", index_html);
+}
 
 void setup() {
  
@@ -57,13 +70,12 @@ void setup() {
 
   });
 
-server.on("/post", HTTP_POST, [](AsyncWebServerRequest * request){
-    
-    if (ON_STA_FILTER(request)) {
-      request->send(200, "text/plain", "Post Registered");
-      return;
-    }
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest * request) {
+    request->send_P(200, "text/html", index_html);
   });
+  
+  server.on("/action", HTTP_POST, action);
+  server.on("/post", HTTP_POST, action);
 
   server.on("/light", HTTP_GET, [](AsyncWebServerRequest * request) {
  
